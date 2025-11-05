4ea5e93d81a4ee07344a4deaedc0acb8a9b38ec7
DESKTOP-2TS9UB7\ecp <ecp@mgl.ca>
2025-11-05 16:19:23 -0500

    Inserted new_top.sv and instantiate the former top block diagram. Rebuilds NO Errors. Enables me to work at top in SystemVerilog.

---
+++ Sample-Projects/Project-0/FPGA/LiteFury/project/project.srcs/sources_1/imports/HDL/chip_top.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/05/2025 03:25:48 PM
+// Design Name: 
+// Module Name: chip_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module chip_top(
+
+    );
+endmodule
